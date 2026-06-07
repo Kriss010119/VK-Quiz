5ae7f7f298f88d29de79d96b0cbbec42057e5d6f
KrIsS <dardardar717@gmail.com>
2026-06-07 12:41:13 +0300

    feat: frontend, footer

---
+++ frontend/src/components/Footer/Footer.tsx
@@ -0,0 +1,10 @@
+import styles from './Footer.module.css';
+
+export const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <p>Created as a VK-education summer practice</p>
+      <p>© 2026, Moscow</p>
+    </footer>
+  );
+};
